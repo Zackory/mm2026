bb31ae3eddb3a7a4ce4a3706297eadbecab5f19f
Zackory Erickson <zackory@cmu.edu>
2026-01-10 15:42:49 -0500

    Update 05_contact.py

--- stretch_python/05_contact.py
+++ stretch_python/05_contact.py
@@ -2,16 +2,18 @@ import stretch_body.robot
 robot = stretch_body.robot.Robot()
 robot.startup()
 
-robot.base.left_wheel.enable_guarded_mode() # Contact detection is disabled for base by default
-robot.base.right_wheel.enable_guarded_mode()
+# robot.base.left_wheel.enable_guarded_mode() # Contact detection is disabled for base by default
+# robot.base.right_wheel.enable_guarded_mode()
 
-# Stops arm at 50% max current detected (e.g. collides with a person)
+# Stops arm at 30% and lift at 50% max current detected (e.g. collides with a person)
 robot.arm.move_to(0.5, contact_thresh_pos=30, contact_thresh_neg=-30)
+robot.lift.move_to(0.9, contact_thresh_pos=50, contact_thresh_neg=-50)
 # Stop the base if it collides into a wall
-robot.base.translate_by(0.5, contact_thresh=(50, -50))
+# robot.base.translate_by(0.5, contact_thresh=5)
 
 robot.push_command()
 robot.arm.wait_until_at_setpoint(timeout=5.0)
-robot.base.wait_until_at_setpoint(timeout=1.0)
+robot.lift.wait_until_at_setpoint(timeout=5.0)
+# robot.base.wait_until_at_setpoint(timeout=1.0)
 
 robot.stop()
